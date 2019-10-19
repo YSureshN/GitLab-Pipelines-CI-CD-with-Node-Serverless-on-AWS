1fd36db0d855550d5b85db9e07e12a58cd5f804a
Martin Burolla <mburolla@gmail.com>
2019-10-19 14:05:02 -0500

    Add test

---
+++ test/testBusinessLogic.js
@@ -0,0 +1,14 @@
+//
+// Name: testBusinessLogic.js
+// Auth: Martin Burolla
+// Date: 10/19/2019
+// Desc: Simple test stub.
+//
+
+const assert = require('assert');
+
+describe('Test', function() {
+  it('should perform simple test', function() {
+    assert.equal(1, 1);
+  });
+});
